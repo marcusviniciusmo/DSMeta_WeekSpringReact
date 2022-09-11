662d252ce1abbf8917f0e9988cd3a6e700c72c6a
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-09-11 18:28:11 +0100

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -15,11 +15,15 @@ function SalesCard() {
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales`)
+
+    const minDate = dateMin.toISOString().slice(0, 10);
+    const maxDate = dateMax.toISOString().slice(0, 10);
+
+    axios.get(`${BASE_URL}/sales?minDate=${minDate}&maxDate=${maxDate}`)
       .then((response) => {
         setSales(response.data.content);
       });
-  }, []);
+  }, [dateMin, dateMax]);
 
   return (
     <div className="dsmetaCard">
