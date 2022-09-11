9a9234acc8e608aabf95ec1c962a7343a1cdc2f7
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-09-11 18:49:49 +0100

    Send notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import NotificationIcon from '../../assets/img/notificationIcon.svg';
+import { BASE_URL } from '../../utils/Api';
 import './styles.css';
 
-function NotificationButton() {
+type Props = {
+  saleId: number;
+};
+
+function NotificationButton({ saleId }: Props) {
+  const handleClick = (id: number) => {
+    axios.get(`${BASE_URL}/sales/${id}/notification`)
+      .then((response) => {
+        console.log('Sucesso')
+      });
+  };
+
   return (
-    <div className="dsmetaRedBtn">
+    <div className="dsmetaRedBtn" onClick={() => handleClick(saleId)}>
       <img src={NotificationIcon} alt="Notificar" />
     </div>
   );
